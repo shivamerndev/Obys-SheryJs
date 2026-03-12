d30572a6c1f9b815a8a548c2936e2b4991cdeef8
Shivam Kumar <shivam.ownwork@gmail.com>
2026-03-13 02:50:18 +0530

    navbar fixed

--- script.js
+++ script.js
@@ -78,7 +78,6 @@ function crsrmagnet() {
     document.addEventListener("mousemove", function (dets) {
         crsr.style.left = dets.x + "px"
         crsr.style.top = dets.y + "px"
-        crsr.style.transform = translate(`-${dets.x}%`, `-${dets.y}%`)
     });
 
     Shery.makeMagnet("nav h1,i");
